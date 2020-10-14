78827f08ca67550cf9e7b508e370b79301ec918a
Svetlana <gubinas83@gmail.com>
2020-10-15 00:07:51 +0300

    init: start brackets task, implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let myMap = new Map();
+
+    let count = {};
+    for (let i of bracketsConfig) {
+        myMap.set(`${i[0]}`, `${i[1]}`);
+        if (i[0] === i[1]) {
+            count[i[0]] = 0;
+        }
+    }
+
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        let item = str[i];
+
+        if (myMap.has(item) && myMap.get(item) === item) {
+            if (count[item] % 2 === 0) {
+                stack.push(item);
+            } else {
+                if (myMap.get(stack.pop()) !== item) {
+                    return false;
+                }
+            }
+            count[item]++;
+        } else if (myMap.has(item) && item !== '|') {
+            stack.push(item);
+        } else {
+            if (myMap.get(stack.pop()) !== item) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 }
